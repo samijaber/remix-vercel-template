e2cbd221753ba42d429f5382192b8c4eeba1d400
Sami Jaber <jabersami@gmail.com>
2024-04-17 19:26:36 -0300

    f

--- app/routes/($slug)._index.tsx
+++ app/routes/($slug)._index.tsx
@@ -1,51 +1,47 @@
 // ($slug)._index.tsx
-// import {
-//   fetchOneEntry,
-//   isEditing,
-//   isPreviewing,
-//   Content,
-//   getBuilderSearchParams,
-// } from "@builder.io/sdk-react";
-// import type { LoaderFunction, LoaderFunctionArgs } from "@remix-run/node";
-// import { useLoaderData } from "@remix-run/react";
+import {
+  fetchOneEntry,
+  isEditing,
+  isPreviewing,
+  getBuilderSearchParams,
+} from "@builder.io/sdk-react";
+import type { LoaderFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
 
-// export const loader = async ({ params, request }: LoaderFunctionArgs) => {
-//   const url = new URL(request.url);
-//   const apiKey = "f1a790f8c3204b3b8c5c1795aeac4660"; // Replace with your actual API key
+export const loader = async ({ params, request }: LoaderFunctionArgs) => {
+  const url = new URL(request.url);
+  const apiKey = "f1a790f8c3204b3b8c5c1795aeac4660"; // Replace with your actual API key
 
-//   const urlPath = `/${params["slug"] || ""}`;
+  const urlPath = `/${params["slug"] || ""}`;
 
-//   console.log("fetching for ", urlPath);
+  console.log("fetching for ", urlPath);
 
-//   const page = await fetchOneEntry({
-//     model: "page",
-//     apiKey: apiKey,
-//     options: getBuilderSearchParams(url.searchParams),
-//     userAttributes: { urlPath },
-//   });
+  const page = await fetchOneEntry({
+    model: "page",
+    apiKey: apiKey,
+    options: getBuilderSearchParams(url.searchParams),
+    userAttributes: { urlPath },
+  });
 
-//   console.log("page", page);
+  console.log("page", page);
 
-//   const isEditingOrPreviewing = isEditing() || isPreviewing();
+  const isEditingOrPreviewing = isEditing() || isPreviewing();
 
-//   if (!page && !isEditingOrPreviewing) {
-//     throw new Response("Page Not Found", {
-//       status: 404,
-//       statusText: "Page not found in Builder.io",
-//     });
-//   }
+  if (!page && !isEditingOrPreviewing) {
+    throw new Response("Page Not Found", {
+      status: 404,
+      statusText: "Page not found in Builder.io",
+    });
+  }
 
-//   return { page };
-// };
+  return { page };
+};
 
 // Define and render the page.
 export default function Page() {
   // Use the useLoaderData hook to get the Page data from `loader` above.
-  // const { page } = useLoaderData<typeof loader>();
-  const page = {
-    name: "test",
-    id: "123",
-  };
+  const { page } = useLoaderData<typeof loader>();
+
   return (
     <div>
       Hello
